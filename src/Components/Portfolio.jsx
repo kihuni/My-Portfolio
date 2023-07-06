a5a75832065c3ac6895a1b7b03ad29e79fa97d25
Stephen kihuni <64348925+kihuni@users.noreply.github.com>
2023-07-06 05:23:08 +0000

    modified portfolio file

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/portfolio.png";
+import image from "../images/tool.jpg";
 
-const imageAltText = "adult standing";
+const imageAltText = "laptop and tools";
 
 /**
  * Project list
